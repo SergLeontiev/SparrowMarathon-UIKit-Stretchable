ab9f60c96597a4e4e3adacbf4d60594c9aa807ef
Sergey Leontiev <translatertrans@gmail.com>
2024-11-19 17:29:43 +0100

    Init project

---
+++ SparrowMarathon-UIKit-Stretchable/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SparrowMarathon-UIKit-Stretchable
+//
+//  Created by Sergey Leontiev on 19.11.24..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
